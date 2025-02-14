cbdc890e25700a747eae539010dfdfcf8b0df0d0
huldil <92667550+huldil@users.noreply.github.com>
2025-02-14 16:23:03 +0300

    Update check-domains.js

--- api/check-domains.js
+++ api/check-domains.js
@@ -8,9 +8,6 @@ export default async function handler(req, res) {
     try {
         const apiUrl = new URL('https://domainr.p.rapidapi.com/v2/search');
         apiUrl.searchParams.set('query', name);
-        apiUrl.searchParams.set('defaults', 'club,coffee');
-        apiUrl.searchParams.set('registrar', 'dnsimple.com');
-        apiUrl.searchParams.set('location', 'de');
 
         const response = await fetch(apiUrl.toString(), {
             method: 'GET',
@@ -25,8 +22,8 @@ export default async function handler(req, res) {
         }
 
         const data = await response.json();
-        
-        // Set CORS headers
+        console.log("API Response:", data);  // Debugging line
+
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.status(200).json(data);
     } catch (error) {
